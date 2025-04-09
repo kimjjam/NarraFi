0ab3c1abc1e3fa99eb593d76f60dfbd03e971560
kimjjam <94438160+kimjjam@users.noreply.github.com>
2025-04-09 11:39:56 +0900

    네이버 로그인 PrivateRoute.jsx 수정

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,7 +2,10 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
+    // 토큰을 로컬스토리지에서 가져옴
     const token = localStorage.getItem("token");
+
+    // 토큰이 있으면 자식 컴포넌트를 보여주고, 없으면 로그인 페이지로 리디렉션
     return token ? children : <Navigate to="/" replace />;
 };
 
